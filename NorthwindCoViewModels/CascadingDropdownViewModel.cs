084f153a2c64a8c145629254959a2471b96eae47
Marius <mariuskoen@gmail.com>
2021-01-23 21:39:40 +1100

    Check in

---
+++ NorthwindCoViewModels/CascadingDropdownViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NorthwindCoModels;
+
+namespace NorthwindCoViewModels
+{
+    public class CascadingDropdownViewModel
+    {
+        public List<Country> AllCountries { get; set; }
+        public List<State> AllStates { get; set; }
+        public List<Town> AllTowns { get; set; }
+
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+        public int TownId { get; set; }
+
+        public CascadingDropdownViewModel LoadModel()
+        {
+            var model = new CascadingDropdownViewModel
+            {
+                AllCountries = new List<Country>(),
+                AllStates = new List<State>(),
+                AllTowns = new List<Town>()
+            };
+
+            var country = new Country { Id = 1, Description = "Australia" };
+            model.AllCountries.Add(country);
+
+            country = new Country { Id = 2, Description = "Brazil" };
+            model.AllCountries.Add(country);
+
+            country = new Country { Id = 3, Description = "Germany" };
+            model.AllCountries.Add(country);
+
+            country = new Country { Id = 4, Description = "Mexico" };
+            model.AllCountries.Add(country);
+
+            var state = new State { Id = 1, Description = "New South Wales", CountryId = 1 };
+            model.AllStates.Add(state);
+
+            state = new State { Id = 2, Description = "Queensland", CountryId = 1 };
+            model.AllStates.Add(state);
+
+            state = new State { Id = 3, Description = "Victoria", CountryId = 1 };
+            model.AllStates.Add(state);
+
+            state = new State { Id = 4, Description = "South Australia", CountryId = 1 };
+            model.AllStates.Add(state);
+
+            var town = new Town { Id = 1, Description = "Berrara", StateId = 1};
+            model.AllTowns.Add(town);
+
+            town = new Town { Id = 2, Description = "Cawongla", StateId = 1 };
+            model.AllTowns.Add(town);
+
+            town = new Town { Id = 3, Description = "Gerogery", StateId = 1 };
+            model.AllTowns.Add(town);
+
+            town = new Town { Id = 4, Description = "Kikoira", StateId = 1 };
+            model.AllTowns.Add(town);
+
+            return model;
+        }
+    }
+}
---
+++ NorthwindCoWeb/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthwindCoModels;
+using NorthwindCoViewModels;
+
+namespace NorthwindCoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Country> _countries = new List<Country>();
+        private List<State> _states = new List<State>();
+        private List<Town> _towns = new List<Town>();
+
+        public ActionResult Index()
+        {
+            var model = new CascadingDropdownViewModel();
+
+            return View(model.LoadModel());
+        }
+
+        public ActionResult LoadCountriesList()
+        {
+            var model = new CascadingDropdownViewModel();
+
+            model = model.LoadModel();
+
+            _countries = model.AllCountries;
+
+            if (HttpContext.Request.IsAjaxRequest())
+                return Json(new SelectList(
+                        _countries,
+                        "Id",
+                        "Description"), JsonRequestBehavior.AllowGet
+                );
+            return null;
+        }
+
+        public ActionResult LoadStateList(int id)
+        {
+            var model = new CascadingDropdownViewModel();
+
+            model = model.LoadModel();
+
+            _states = model.AllStates.Where(x => x.CountryId == id).ToList();
+
+            if (HttpContext.Request.IsAjaxRequest())
+                return Json(new SelectList(
+                        _states,
+                        "Id",
+                        "Description"), JsonRequestBehavior.AllowGet
+                );
+
+            return null;
+        }
+
+        public ActionResult LoadTownList(int id)
+        {
+            var model = new CascadingDropdownViewModel();
+
+            model = model.LoadModel();
+
+            _towns = model.AllTowns.Where(x => x.StateId == id).ToList();
+
+            if (HttpContext.Request.IsAjaxRequest())
+                return Json(new SelectList(
+                        _towns,
+                        "Id",
+                        "Description"), JsonRequestBehavior.AllowGet
+                );
+
+            return null;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
